049817f30d0e7a5f95cd3d7f0ca3606afff1b280
berkergucur <brkr.gcr@gmail.com>
2016-03-11 23:27:54 +0200

    develop branch created.

--- gandalf/src/test/java/com/github/brkr/gandalf/GandalfTest.java
+++ gandalf/src/test/java/com/github/brkr/gandalf/GandalfTest.java
@@ -1,14 +1,20 @@
 package com.github.brkr.gandalf;
 
+import com.github.brkr.gandalf.utils.RuleTest;
+
 import org.junit.Before;
 
 /**
- * @Author :  berkergucur (brkr.gcr@gmail.com) - 06/03/16.
+ * @author :  berkergucur (brkr.gcr@gmail.com) - 06/03/16.
  */
-public class GandalfTest {
+public class GandalfTest extends RuleTest {
 
     @Before
     public void setUp() throws Exception {
+    }
+
+    public void maxAndMinChainShouldReturn(){
+        
     }
 
 }
